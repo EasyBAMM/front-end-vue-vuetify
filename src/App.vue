291f35d5d183ee2557004794675854dbf96cb92c
EasyBAMM <huhjb1020@naver.com>
2021-11-04 14:30:13 +0900

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,84 @@
+<!-- 컴포넌트 UI 정의 -->
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app hide-on-scroll>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer
+      ><v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6"> Application </v-list-item-title>
+              <v-list-item-subtitle>Vue + Vuetify</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+          <v-list-item to="/">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item to="/about">
+            <v-list-item-icon>
+              <v-icon>mdi-help-box</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>About</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+    <v-footer color="secondary" dark> <div class="mx-auto">H.J.B</div></v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  // 컴포넌트의 대표이름(devtools에 나오는 이름)
+  name: "App",
+  // 추가하고 싶은 컴포넌트 등록(import something from "/path")
+  components: {},
+  // 컴포넌트 데이터 정의
+  data() {
+    return { drawer: false, group: null };
+  },
+  // 컴포넌트 메소드 정의
+  methods: {},
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<!-- 컴포넌트 스타일 정의 -->
+<style scoped>
+/* scoped: 전역 범위X, 컴포넌트 범위 */
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-btn depressed @click="handleBtn"> 요청 보냄 시작 </v-btn>
+    <v-btn depressed color="error" @click="handleBtn2"> 요청 보냄 종료 </v-btn>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  // 컴포넌트의 대표이름(devtools에 나오는 이름)
+  name: "App",
+  // 추가하고 싶은 컴포넌트 등록(import something from "/path")
+  components: {},
+  // 컴포넌트 데이터 정의
+  data() {
+    return { startInterval: null };
+  },
+  // 컴포넌트 메소드 정의
+  methods: {
+    handleBtn() {
+      this.startInterval = setInterval(async () => {
+        try {
+          const respone = await axios.get(
+            "https://www.google.com",
+          );
+          console.log(respone);
+        } catch (error) {
+          console.log(error);
+        }
+      }, 3000);
+    },
+    handleBtn2() {
+      clearInterval(this.startInterval);
+    },
+  },
+};
+</script>
